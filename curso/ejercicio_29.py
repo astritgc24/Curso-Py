34973ad2a25e0bf9fd091ba1b336e7595cf8083b
Astrit Lariza Garcia Castillo <larizagc242003@gmail.com>
2024-10-14 15:00:00 -0600

    curso

---
+++ curso/ejercicio_29.py
@@ -0,0 +1,21 @@
+#!/bin/python
+# coding=utf-8
+
+from PyZenity import Question
+from PyZenity import InfoMessage
+
+
+#Question
+def linux_instalado():
+    if Question('Tienes instalado Linux?',width=250,height=140,title="Python for Beginners"): #nos devolverá un booleano
+        InfoMessage('Bienvenido al udo Linuxero',width=250,height=140,title="Python for Beginners")
+    else:
+        InfoMessage('Deberias instalar Linux',width=250,height=140,title="Python for Beginners")
+
+
+def main():
+	linux_instalado()
+
+
+if __name__ == '__main__':
+    main()
