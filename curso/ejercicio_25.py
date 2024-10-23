b983c36d88af647272275e86704b37405cb4dd91
Astrit Lariza Garcia Castillo <larizagc242003@gmail.com>
2024-10-23 15:00:00 -0600

    curso

---
+++ curso/ejercicio_25.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from lista import *
+from PyZenity import InfoMessage
+
+
+def get_item_lista():
+    item = lista()
+    print("Elegiste: ")
+    print("Autor: ",item[0],", Lenguaje: ",item[1],", País:",item[2])
+    autor = item[0]
+
+    if autor == 'Larry Wall':
+    	autor = autor +", \n'Los verdaderos programadores pueden escribir código ensamblador en cualquier lenguaje.'"
+    elif autor == 'James Gosling':
+        autor = autor +" \n'La imitación es la forma más sincera de adulación, muchas gracias.'"
+    else: 
+    	if autor == "Guido van Rossum":
+    		autor = autor +" \n'Python es mejor que cualquier otro lenguaje.'"
+        else:
+            autor = autor +" \n'Hola.'"
+
+
+    InfoMessage("Elegiste: \nAutor: "+autor+"\nLenguaje: "+item[1]+"\nPaís:"+item[2],width=250,height=140,title="Python for Beginners")
+
+
+
+def main():
+	get_item_lista()
+
+
+if __name__ == '__main__':
+    main()
