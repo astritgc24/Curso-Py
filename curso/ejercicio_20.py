6b4710e2c7b094fd7f07b22eaf457da5ea9d06dc
Astrit Lariza Garcia Castillo <larizagc242003@gmail.com>
2024-10-31 15:00:00 -0600

    curso

---
+++ curso/ejercicio_20.py
@@ -0,0 +1,93 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+MAX = 100
+
+
+def iterable():
+    numeros = [5,65,0,-32,111,32,5,18]
+    it = iter(numeros)
+    print("Numeros:")
+    print(next(it))
+    print(next(it))
+    print(next(it))
+    print(next(it))
+    print(next(it))
+    print(next(it))
+
+
+
+#ciclo for
+def ciclo_for():
+    global MAX
+    print("Ciclo for:")
+    for i in range(1,MAX):
+    	if i%3 == 0 and i%5 ==0:
+    		print("No. ",i)
+    print("\n")
+    lista = [1,"2",True, 5, float(2), int("32") ]
+    for i in range(len(lista)):
+        print(i)
+    cont = 0
+    for letra in "Guayaba":
+        print(letra)
+        cont = cont + 1
+    print("Cantidad: ",cont)
+    for x in range(10):
+        print("Hola no.",x)
+    for y in range(0,10):
+        print("Ciao no.",y)
+    rango = range(0,MAX)
+    for z in rango:
+        if z%3==0 and z%5==0:
+            print("Welcome, ",z)
+    cont = 0
+    while cont < MAX:
+        print(cont)
+        cont = cont + 3
+        if cont > 50:
+            break
+    print("Contador: ",cont)
+    
+
+ 
+#ciclo for return
+def ciclo_for_return():
+    global MAX
+    suma = 1.5
+    print("Ciclo for return:")
+    for c in range(2,MAX):
+        suma += c
+        print("Suma parcial = ",suma)
+    return suma
+
+
+
+#ciclo while return                   
+def ciclo_while_return():
+    global MAX
+    print("Ciclo while:")
+    contador, suma = 0,0
+    while contador < MAX:
+        suma += contador
+        contador += 3
+    return suma
+    
+
+def uso_rango():
+    print("Rango:")
+    for num in range(0, 11, 2):
+        print(num)
+
+
+def main():
+    ciclo_for()
+    print("Ciclo for return: ",ciclo_for_return())
+    print("Ciclo while return: ",ciclo_for_return())
+    iterable()
+    uso_rango()
+
+
+
+if __name__ == '__main__':
+    main()
