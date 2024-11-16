1070350ef4efb64a640e312f4faad29fb7fcb6d6
Astrit Lariza Garcia Castillo <larizagc242003@gmail.com>
2024-11-16 15:00:00 -0600

    curso

---
+++ curso/ejercicio_4.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from configparser import ConfigParser
+import psycopg2
+#from config import config
+
+
+def config(filename='database.ini', section='postgresql'):
+    parser = ConfigParser()
+    parser.read(filename)
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+    return db
+    
+
+
+def connect():
+    """ Conexion a la base de datos PostgreSQL """
+    conn = None
+    try:
+        # read connection parameters
+        params = config()
+
+        # connect to the PostgreSQL server
+        print('Conectando a la base de datos PostgreSQL...')
+        conn = psycopg2.connect(**params)
+		
+        # create a cursor
+        cur = conn.cursor()
+        
+	# execute a statement
+        #print('Version de la base de datos PostgreSQL:')
+        #cur.execute('SELECT version()')
+        print("Ejecutando funcion: ")
+        cur.execute('SELECT public."showContacts"()')
+
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
+       
+	    # close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('La conexion a la base de datos PostgreSQL se ha cerrado.')
+
+
+
+def main():
+    connect()
+    
+if __name__ == '__main__':
+    main()
+
+
