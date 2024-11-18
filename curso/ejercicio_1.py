79d56c06bc405ac65b014e0d77480ded37df507f
Astrit Lariza Garcia Castillo <larizagc242003@gmail.com>
2024-11-18 15:00:00 -0600

    curso

---
+++ curso/ejercicio_1.py
@@ -0,0 +1,78 @@
+#!/bin/python
+# coding=utf-8
+
+
+import os
+import csv
+
+
+def leer_archivo():
+	os.chdir(r'/home/fernando/Documentos/testRepositories/python-en-ejemplos/app/chapter0')
+	arch = open('datos.csv')
+	print("Ruta: ",open("datos.csv","r"))
+	try:
+		if open("datos.csv","r"):
+			print("Parece que el archivo existe")
+	except IOError as e:
+		print("Error: ",e)
+
+	try:
+		file = open("datos.csv","r")
+		print(type(file))
+		print("Contenido:",file.readlines())
+	except IOError as e:
+		print("Error: ",e)
+
+
+def lectura_archivo():
+	print("Leer archivo:")
+	os.chdir(r'/home/fernando/Documentos/testRepositories/python-en-ejemplos/app/chapter0')
+	arch = open('datos.csv','r')
+	try:
+		if arch:
+			print("Archivo existe:")
+			print(arch)
+	except IOError as e:
+		print("Error: ",e)
+	finally:
+		lineas = [linea for linea in arch]
+		print("Contenido:")
+		print(lineas)
+		print("Linea 1:")
+		print(lineas[1])
+		print("Linea 2:")
+		print(lineas[2])
+		print("")
+		print(lineas[1].strip())
+		print(lineas[1].strip().split(','))
+		print(lineas[2].strip())
+		print(lineas[2].strip().split(','))
+
+		
+
+def get_funciones_csv():
+	print("Funciones csv:")
+	print(dir(csv))
+	print("")
+	os.chdir(r'/home/fernando/Documentos/testRepositories/python-en-ejemplos/app/chapter0')
+	arch = open('datos.csv','r')
+	try:
+		if arch:
+			print("Archivo existe")
+			reader = csv.reader(arch)
+			header = next(reader)
+			data = [row for row in reader]
+			print("Header: ",header)
+			print("Datos: ",data)
+	except IOError as e:
+		print("Error: ",e)
+	finally:
+		print("Final de bloque")
+
+def main():
+	get_funciones_csv()
+	lectura_archivo()
+	leer_archivo()
+
+if __name__ == '__main__':
+    main()
